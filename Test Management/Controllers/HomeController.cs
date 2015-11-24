db700d83b1bfe43cce5867e0a0be33018f2894d3
Michael Paulsen <mpaulsen@knights.ucf.edu>
2015-11-24 15:32:34 -0500

    Baby Steps
    Completion of test submission process (probably will need to implement
    more validation).
    Added contact info update page, fully functional.
    Fixed some CSS stuff to make the navbar branded and the footer fits
    correctly now.
    Began work on test exception process.

--- Test Management/Controllers/HomeController.cs
+++ Test Management/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,8 +21,8 @@ namespace Test_Management.Controllers
         public ActionResult Index(User prof)
         {
             if (Authenticate(prof))
-                //Session["User"] = prof.Username;
-                Session["User"] = "bdurham";
+                Session["User"] = prof.Username;
+                //Session["User"] = "bdurham";
             else
             {
                 ModelState.AddModelError(String.Empty, "Incorrect Username/Password");
@@ -77,6 +78,7 @@ namespace Test_Management.Controllers
             return View( TVM );
         }
 
+
         public ActionResult LoadTestInfo(TestCourseVM tcvm)
         {
             if (!CheckCookie())
@@ -127,6 +129,18 @@ namespace Test_Management.Controllers
                 ModelState.AddModelError(String.Empty, "Invalid value selected for open date.");
                 return RedirectToAction("LoadTestInfo", tcvm);
             }
+            if (CheckTimes(tcvm.test.StartDate))
+            {
+                ModelState.AddModelError(String.Empty, "Opening time not in range for selected opening day.");
+                return RedirectToAction("LoadTestInfo", tcvm);
+            }
+            if (CheckTimes(tcvm.test.EndDate))
+            {
+                ModelState.AddModelError(String.Empty, "Closing time not in range for selected closing day.");
+                return RedirectToAction("LoadTestInfo", tcvm);
+            }
+
+            
 
             List<string> courseIDs = new List<string>();
             foreach (Course thing in tcvm.courses)
@@ -146,23 +160,161 @@ namespace Test_Management.Controllers
                 //For each course after the first, add the string below with the course ids
                 for (int i = 1; i < courseIDs.Count; i++ )
                 {
-                    query += "OR Course_ID = '" + courseIDs[i] +"' ";
+                    query += "OR Course_ID = '" + courseIDs[i] + "' ";
                 }
             }
 
             query += ")";
 
-            int enrollment = Convert.ToInt32(Models.Database.ScalarString(query));
+            string enrollment = Models.Database.ScalarString(query);
+
+            //Query date's enrollment vs this enrollment (unpublished/verified test enrollment counts) and make sure the date population will still be kosher
+            if (CheckEnrollment(enrollment, tcvm.test.EndDate))
+            {
+                ModelState.AddModelError(String.Empty, "Selected closing date is at capacity.  Please choose another closing date.");
+                return RedirectToAction("LoadTestInfo", tcvm);
+            }
 
             //Query for enrollment first into string variable Enrollment
-
-            query = "INSERT INTO tlTests VALUES('" + tcvm.test.Name + "', '1', '" + tcvm.test.StartDate + "', '" + tcvm.test.EndDate + "', '1', NULL, NULL, '" + tcvm.test.Notes.Replace("'", "''") + "', '0', '" + enrollment + "', '', '1', '" + DateTime.Now + "', '0', NULL, '" + tcvm.test.Length + @"', '0', '0')
+            query = "INSERT INTO tlTests VALUES('" + tcvm.test.Name + "', '1', '" + tcvm.test.StartDate + "', '" + tcvm.test.EndDate + "', '1', NULL, NULL, '" + tcvm.test.Notes.Replace("'", "''") + "', '0', (SELECT Value FROM tlSettings WHERE Property = 'CurrentTerm'), 1, '" + enrollment + "', 0, '1', '" + DateTime.Now + "', '0', NULL, '" + tcvm.test.Length + @"', '0', '0')
                      SELECT SCOPE_IDENTITY()";
-            //Use that OUTPUT thing to get testID back after insert?
 
-            //User can alter dates despite being blacked out, check them and be ready to clear model state and return with model error.
-            //Some kind of confirmation page?
-            return RedirectToAction("NewTest");
+            string insertedID = Models.Database.ScalarString(query);
+
+            foreach (string CID in courseIDs)
+                Models.Database.nonQuery("INSERT INTO tlTests_Courses VALUES('" + insertedID + "', '" + CID + "')");
+
+            foreach (Models.Material mat in tcvm.materialList)
+                if (mat.Quantity)
+                    Models.Database.nonQuery("INSERT INTO tlTests_Test_Materials VALUES ('" + insertedID + "', '" + mat.MaterialID + "', 1, '')");
+
+            //Email Notifications Go Here
+
+            return RedirectToAction("Confirmation");
+        }
+
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateInfo()
+        {
+            if (!CheckCookie())
+                return RedirectToAction("Index");
+
+            string number = Models.Database.ScalarString("SELECT ISNULL(Emergency_Phone, '') FROM tlProfessors WHERE CBAUserName = '" + Session["User"].ToString() + "'");
+            return View("UpdateInfo", null, number);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateInfo(string number)
+        {
+            if (!CheckCookie())
+                return RedirectToAction("Index");
+
+            try{
+                Models.Database.nonQuery("UPDATE tlProfessors SET Emergency_Phone = '" + number + "' WHERE CBAUserName = '" + Session["User"].ToString() + "'");
+            }
+            catch{;}
+            return RedirectToAction("Home");
+        }
+
+        private bool CheckEnrollment(string strRoll, DateTime date)
+        {
+            int enrollment = Convert.ToInt32(strRoll);
+
+            int dateEnrollment = Convert.ToInt32(Models.Database.ScalarString(@"
+                SELECT COUNT(NID) AS 'TotalEnrollment'
+                FROM tlTests T, tlTests_Courses TC, tlStudents_Courses SC
+                WHERE T.Test_ID = TC.Test_ID AND TC.Course_ID = SC.Course_ID AND SC.Enrollment_Status LIKE 'Enrol%'
+                  AND CONVERT(date, Close_Date_Time) = '" + date.Date + "' AND T.Term = SC.Term"));
+
+            int totalEnrollment = enrollment + dateEnrollment;
+
+            DateTime finalsStart = Convert.ToDateTime(Models.Database.ScalarString("SELECT Value FROM tlSettings WHERE Property = 'FinalsOpen'"));
+            DateTime finalsEnd = Convert.ToDateTime(Models.Database.ScalarString("SELECT Value FROM tlSettings WHERE Property = 'FinalsClose'"));
+            bool finals = (date > finalsStart && date < finalsEnd);
+            int enrollmentCap = 0;
+
+            if (finals)
+                enrollmentCap = Convert.ToInt32(Models.Database.ScalarString("SELECT Value FROM tlSettings WHERE Property = 'FinalsCap'"));
+            else if (date.DayOfWeek == DayOfWeek.Friday || date.DayOfWeek == DayOfWeek.Saturday)
+                enrollmentCap = Convert.ToInt32(Models.Database.ScalarString("SELECT Value FROM tlSettings WHERE Property = 'WeekendCap'"));
+            else
+                enrollmentCap = Convert.ToInt32(Models.Database.ScalarString("SELECT Value FROM tlSettings WHERE Property = 'WeekdayCap'"));
+
+            if (totalEnrollment > enrollmentCap)
+                return true;
+
+            return false;
+        }
+
+        //Returns true if there's a problem with the times selected for a given date.
+        private bool CheckTimes(DateTime date)
+        {
+            DateTime finalsStart = Convert.ToDateTime("01/01/1900");
+            DateTime finalsEnd = Convert.ToDateTime("01/01/1900");
+            DateTime startTime = Convert.ToDateTime("01/01/1900");
+            DateTime startTimeFriday = Convert.ToDateTime("01/01/1900");
+            DateTime startTimeSaturday = Convert.ToDateTime("01/01/1900");
+            DateTime endTime = Convert.ToDateTime("01/01/1900");
+            DateTime endTimeFriday = Convert.ToDateTime("01/01/1900");
+            DateTime endTimeSaturday = Convert.ToDateTime("01/01/1900");
+            SqlConnection con = new SqlConnection(Models.Database.ConnectionString);
+            string query = @"SELECT Property, Value FROM tlSettings WHERE Property = 'FinalsOpen' OR Property = 'FinalsClose' OR Property = 'TestCalendar:StartTime' 
+                             OR Property = 'TestCalendar:StartTimeFriday' OR Property = 'TestCalendar:StartTimeSaturday' OR Property = 'TestCalendar:EndTime' 
+                             OR Property = 'TestCalendar:EndTimeFriday' OR Property = 'TestCalendar:EndTimeSaturday'
+                             ORDER BY Property ASC";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                if (rdr[0].ToString() == "FinalsOpen")
+                    finalsStart = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "FinalsClose")
+                    finalsEnd = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:StartTime")
+                    startTime = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:StartTimeFriday")
+                    startTimeFriday = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:StartTimeSaturday")
+                    startTimeSaturday = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:EndTime")
+                    endTime = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:EndTimeFriday")
+                    endTimeFriday = Convert.ToDateTime(rdr[1].ToString());
+                if (rdr[0].ToString() == "TestCalendar:EndTimeSaturday")
+                    endTimeSaturday = Convert.ToDateTime(rdr[1].ToString());
+
+            }
+            con.Close();
+
+            bool finals = false;
+            if (date > finalsStart && date < finalsEnd)
+                finals = true;
+            
+            if (date.DayOfWeek == DayOfWeek.Friday && !finals)
+            {
+                if (date.Hour < startTimeFriday.Hour || (date.Hour == endTimeFriday.Hour && date.Minute > endTimeFriday.Minute) || date.Hour > endTimeFriday.Hour)
+                    return true;
+            }
+            else if (date.DayOfWeek == DayOfWeek.Saturday && !finals)
+            {
+                if (date.Hour < startTimeSaturday.Hour || (date.Hour == startTimeSaturday.Hour && date.Minute < startTimeSaturday.Minute) || date.Hour > endTimeSaturday.Hour || (date.Hour == endTimeSaturday.Hour && date.Minute > endTimeSaturday.Minute) )
+                    return true;
+            }
+            else if (date.DayOfWeek == DayOfWeek.Sunday)
+                return true;
+            else
+            {
+                if (date.Hour < startTime.Hour)
+                    return true;
+            }
+
+            //No errors found
+            return false;
         }
 
         public bool Authenticate(User prof)
---
+++ Test Management/Models/ExceptionVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_Management.Models
+{
+    public class ExceptionVM
+    {
+        public string NID { get; set; }
+        public string TID { get; set; }
+        public DateTime Open { get; set; }
+        public DateTime Close { get; set; }
+        
+
+        public void Insert()
+        {
+            //Check that date/time isn't during a date exception and that the time submitted falls within the times the lab is open.
+
+            Models.Database.nonQuery("INSERT INTO tlTests_Exceptions VALUES ('" + NID + "', '" + TID + "', '" + Open + "', '" + Close + "', 1, '" + DateTime.Now + "', 0)");
+
+            //Email notifications.
+        }
+    }
+}
